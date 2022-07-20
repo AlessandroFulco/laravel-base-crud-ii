40a84d0a411022050ae04f02a67a8b7a24a791ec
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-07-20 12:57:43 +0200

    adding error edit

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -8,33 +8,40 @@
         @method('PUT')
         @csrf
 
+        @if($errors->any())
+            <div class="alert alert-danger mt-2">
+                @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                @endforeach
+            </div>
+
         <div class="mb-3">
-          <label for="title" class="form-label">Title</label>
-          <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title)}}">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control" id="description" name="description" value="{{$comic->description}}">
+            <input type="text" class="form-control" id="description" name="description" value="{{old('description', $comic->description)}}">
         </div>
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
+            <input type="text" class="form-control" id="thumb" name="thumb" value="{{old('thumb', $comic->thumb)}}">
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{$comic->price}}">
+            <input type="text" class="form-control" id="price" name="price" value="{{old('price', $comic->price)}}">
         </div>
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+            <input type="text" class="form-control" id="series" name="series" value="{{old('series', $comic->series)}}">
         </div>
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale_date</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{$comic->sale_date}}">
+            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{old('sale_date', $comic->sale_date)}}">
         </div>
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+            <input type="text" class="form-control" id="type" name="type" value="{{old('type', $comic->type)}}">
         </div>
 
         <button type="submit" class="btn btn-primary">Update</button>
