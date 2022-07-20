564e336144134068f9ca40f6db0365b2090969b1
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-07-20 12:50:01 +0200

    adding previous value form create

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -9,7 +9,7 @@
 
     <div class="mb-3">
         <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" name="title">
+        <input type="text" class="form-control" id="title" name="title" value="{{ old('title')}}">
             {{-- validation --}}
             @error('title')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -19,7 +19,7 @@
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
-        <input type="text" class="form-control" id="description" name="description">
+        <input type="text" class="form-control" id="description" name="description" value="{{ old('description')}}>
         {{-- validation --}}
         @error('description')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -29,7 +29,7 @@
     </div>
     <div class="mb-3">
         <label for="thumb" class="form-label">Thumb</label>
-        <input type="text" class="form-control" id="thumb" name="thumb">
+        <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb')}}>
         {{-- validation --}}
         @error('thumb')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -39,7 +39,7 @@
     </div>
     <div class="mb-3">
         <label for="price" class="form-label">Price</label>
-        <input type="text" class="form-control" id="price" name="price">
+        <input type="text" class="form-control" id="price" name="price" value="{{ old('price')}}>
         {{-- validation --}}
         @error('price')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -49,7 +49,7 @@
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Series</label>
-        <input type="text" class="form-control" id="series" name="series">
+        <input type="text" class="form-control" id="series" name="series" value="{{ old('series')}}>
         {{-- validation --}}
         @error('series')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -59,7 +59,7 @@
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">Sale_date</label>
-        <input type="text" class="form-control" id="sale_date" name="sale_date">
+        <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date')}}>
         {{-- validation --}}
         @error('sale_date')
                 <div class="alert alert-danger mt-2" role="alert">
@@ -69,7 +69,7 @@
     </div>
     <div class="mb-3">
         <label for="type" class="form-label">Type</label>
-        <input type="text" class="form-control" id="type" name="type">
+        <input type="text" class="form-control" id="type" name="type" value="{{ old('type')}}>
         {{-- validation --}}
         @error('type')
                 <div class="alert alert-danger mt-2" role="alert">
